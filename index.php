ff540894a61f59fcad61e8cc6d92c7ab71f28f9c
wachidaljawi <rwachid17@gmail.com>
2020-03-16 15:53:56 +0700

    create row content

--- index.php
+++ index.php
@@ -11,7 +11,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-8">
+            <h2>Company Profile</h2>
+            <p><?php $isi_content ?></p>
+        </div>
+        <div class="col-4">
+            <h2>Learn More</h2>
+        </div>
+    </div>
+</div>
 
 
  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
